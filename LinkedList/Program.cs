8d811df028963dcd4482512a08c441b6852f7b6b
Ludwig Lennartsson <ludwig.lennartsson@skola.sala.se>
2024-09-25 15:14:23 +0200

    duplicates är klar

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -40,6 +40,52 @@ namespace LinkedList
 
             minLista.RemoveFirst(); 
             Console.WriteLine(minLista.First.Value);
+
+
+
+            // [34,21,34,98, 98] => [34,98]
+            List<int> testList = new List<int> { 34, 21, 34, 98, 98 };
+            List<int> dupList = Duplicates(testList);
+            Console.WriteLine("Input List: " + String.Join(",", testList));
+            Console.WriteLine("Output List: " + String.Join(",", dupList));
+
+            // [apa,bpa,cpa,bpa] => [bpa]
+            List<string> testList2 = new List<string> { "apa", "bpa", "cpa", "bpa" };
+            List<string> dupList2 = Duplicates(testList2);
+            Console.WriteLine("Input List 2: " + String.Join(",", testList2));
+            Console.WriteLine("Output List 2: " + String.Join(",", dupList2));
+        }
+
+        public static List<T> Duplicates<T>(List<T> minLista)
+        {
+            List<T> result = new List<T>();
+
+            // ett dictionary som håller koll på hur många ggr som någonting har hittats i 'minlista'
+            Dictionary<T, int> occurrences = new Dictionary<T, int>();
+
+            // kollar om grejen finns en gång
+            foreach (T t in minLista)
+            {
+                if (occurrences.ContainsKey(t))
+                {
+                    occurrences[t]++;
+                }
+                else
+                {
+                    occurrences[t] = 1;
+                }
+            }
+
+            // kollar om grejen finns mer än en gång
+            foreach (var entry in occurrences)
+            {
+                if (entry.Value > 1 && !result.Contains(entry.Key))
+                {
+                    result.Add(entry.Key);
+                }
+            }
+
+            return result;
         }
     }
 }
